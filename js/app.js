b8632cd004b653ab4867f6c0084fa0a49851a893
rashidul191 <rashidul.191cse.gub@gmail.com>
2022-04-19 00:09:48 +0600

    load and display all phone

--- js/app.js
+++ js/app.js
@@ -1,5 +1,42 @@
 // search phone button
-const searchPhone = () =>{
-    
-}
+const searchPhone = () => {
+  const searchFiled = document.getElementById("search-filed");
+  const searchText = searchFiled.value.toLowerCase();
+  //console.log(searchText);
+  if (searchText != "") {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
 
+    fetch(url)
+      .then((res) => res.json())
+      .then((phone) => displayPhone(phone.data));
+  } else {
+    alert("Error !!! empty search filed");
+  }
+  // clear search filed
+  searchFiled.value = "";
+};
+
+// display all phone
+const displayPhone = (phones) => {
+  // console.log(phones)
+   const section = document.getElementById("display-phone");
+   for(const phone of phones){
+       console.log(phone)
+    const div = document.createElement("div");
+    div.classList.add("col");  
+    div.innerHTML = `
+          <div class="card">
+          <img class="img-fluid h-25" src="${phone.image}" class="card-img-top" alt="...">
+          <div class="card-body">
+              <h5 class="card-title">${phone.phone_name}</h5>
+              <p class="card-text">${phone.brand}</p>
+          </div>
+          </div>
+  `;
+
+  section.appendChild(div)
+   }
+
+
+  
+};
