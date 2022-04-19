e583e55d55af297b22c4db79f152be15378a2448
rashidul191 <rashidul.191cse.gub@gmail.com>
2022-04-19 18:07:18 +0600

    create single product details

--- js/app.js
+++ js/app.js
@@ -20,26 +20,23 @@ const searchPhone = () => {
   // clear search filed
   searchFiled.value = "";
 };
-
 // call display all phone
 const callDisplayAllPhone = (phone) => {
-  // console.log(phone.phone_name);
   const div = document.createElement("div");
   div.classList.add("col");
   return (div.innerHTML = `
       <div class="card">
-        <img class="img-fluid h-25" src="${phone.image}" class="card-img-top" alt="...">
+        <img class="img-fluid h-25" src="${phone.image}" class="card-img-top" alt="Images">
         <div class="card-body">
             <h5 class="card-title">${phone.phone_name}</h5>
             <p class="card-text">${phone.brand}</p>
-        <div class="text-center">
-            <button onclick="loadPhoneDetails(${phone.slug})" class="btn btn-info mb-4">Show Details</button>
-        </div>         
+          <div class="text-center">
+            <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-info mb-4">Show Details</button>
+          </div>         
         </div>      
       </div>
 `);
 };
-
 // display all phone
 const displayPhone = (phones) => {
   const section = document.getElementById("display-phone");
@@ -49,53 +46,49 @@ const displayPhone = (phones) => {
   spinner.style.display = "block";
   let count = 0;
   for (const phone of phones) {
-    console.log(phone);
     count++;
     if (count < 20) {
       const div = document.createElement("div");
       div.classList.add("col");
-      // div.innerHTML = `
-      //     <div onclick="loadPhoneDetails(${phone.slug})" class="card">
-      //         <img class="img-fluid h-25" src="${phone.image}" class="card-img-top" alt="...">
-      //         <div class="card-body">
-      //            <h5 class="card-title">${phone.phone_name}</h5>
-      //           <p class="card-text">${phone.brand}</p>
-      //         </div>
-      //     </div>
-      // `;
       div.innerHTML = callDisplayAllPhone(phone);
       section.appendChild(div);
       // spinner none
       spinner.style.display = "none";
     } else if (count == 20) {
       const div = document.createElement("div");
-      // div.classList.add("col");
-      //     div.innerHTML = `
-      //         <div class="card">
-      //         <img class="img-fluid h-25" src="${phone.image}" class="card-img-top" alt="...">
-      //         <div class="card-body">
-      //             <h5 class="card-title">${phone.phone_name}</h5>
-      //             <p class="card-text">${phone.brand}</p>
-      //         </div>
-      //         </div>
-      // `;
+      div.classList.add("col");
       div.innerHTML = callDisplayAllPhone(phone);
       section.appendChild(div);
       spinner.style.display = "none";
-
       seeAllBtn.style.display = "block";
-      // console.log(count);
+ 
     }
   }
 };
 
-// Load Phone Details
-const loadPhoneDetails = (phoneId) => {
-  console.log("load phone details click done.", phoneId);
-
+// Load single product Details
+const loadPhoneDetails = async (phoneId) => {
   const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
-  console.log(url);
-  // const res = await fetch(url);
-  // const data = await res.json()
-  // console.log(data)
+  const res = await fetch(url);
+  const product = await res.json();
+  singleProductDetails(product.data);
+};
+
+// display single product details
+const singleProductDetails = (productDetails) => {
+  console.log(productDetails.name);
+  const section = document.getElementById("single-product-details");
+  // clear single product details
+  section.textContent = "";
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <div class="card">
+      <img class="img-fluid h-25" src="${productDetails.image}" class="card-img-top" alt="Images">
+      <div class="card-body">
+          <h5 class="card-title">${productDetails.name}</h5>
+          <p class="card-text">${productDetails.brand}</p>              
+      </div>      
+    </div>
+    `;
+  section.appendChild(div);
 };
